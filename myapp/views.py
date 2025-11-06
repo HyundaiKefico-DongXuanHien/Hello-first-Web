3077bb5601a9990a5dc7cb7504b0bd8e3e398924
HyundaiKefico-DongXuanHien <xuanhien.dong@hyundai-kefico.com>
2025-11-06 12:04:37 +0900

    Version2: Add tracking data from DB and displau

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 # ============================================================================
 # FILE NAME     : myapp/views.py
 # AUTHOR        : DONG XUAN HIEN
@@ -10,5 +6,19 @@ from django.shortcuts import render
 # HISTORY       : 05/11/2025
 # ============================================================================
 
-def index(request):
-    return render(request, 'index.html')
+from django.shortcuts import render
+from .models import issue_data_table, issue_data_table_KVHS
+
+def task_list(request):
+    query = request.GET.get('search', '')  #get value from search bar, if not --> default is blank string
+    if query:
+        table1 = issue_data_table.objects.filter(assignee__icontains=query)
+        table2 = issue_data_table_KVHS.objects.filter(assignee__icontains=query)
+    else:
+        table1 = issue_data_table.objects.all()
+        table2 = issue_data_table_KVHS.objects.all()
+
+    # Merge data from 2 table
+    tasks = list(table1) + list(table2)
+    return render(request, 'task_list.html', {'tasks': tasks, 'query': query})
+
--- simple_web/settings.py
+++ simple_web/settings.py
@@ -76,7 +76,8 @@ WSGI_APPLICATION = 'simple_web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database.db',  # cursor to available files
     }
 }
 
